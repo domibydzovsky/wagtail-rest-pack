2a32ae6829b7b13d96dd01c2f816847b457f97ae
Dominik Bydžovský <dominikbydzovsky94@gmail.com>
2021-05-29 10:15:50 +0200

    custom tags endpoints

--- src/wagtail_rest_pack/custom_tag/view.py
+++ src/wagtail_rest_pack/custom_tag/view.py
@@ -1,27 +1,18 @@
 from wagtail.api.v2.utils import BadRequestError
 from wagtail.api.v2.views import BaseAPIViewSet
 from wagtail.core.models import Page
-
-from wagtail_rest_pack.page_banner.serializers import RFBanneredChildrenSerializer
+from rest_framework import generics
 from .models import PageTag
+from rest_framework import serializers
 
-class FakeModel:
-    pass
+class TagSerializer(serializers.ModelSerializer):
+    tag = serializers.StringRelatedField()
+    class Meta:
+        model = PageTag
+        fields = ['tag']
 
-class ObjectsByTagViewset(BaseAPIViewSet):
-    known_query_parameters = BaseAPIViewSet.known_query_parameters.union(['with_tags'])
-    model = FakeModel
 
-    def listing_view(self, request):
-        return super(ObjectsByTagViewset, self).listing_view(request)
-
-    def get_serializer_class(self):
-        return RFBanneredChildrenSerializer
-
-    def get_queryset(self):
-        tags = self.request.query_params.get('with_tags', '').split(',')
-        if not tags:
-            raise BadRequestError('Query paramater `s` must be provided')
-        if len(tags) > 5:
-            raise BadRequestError('Only 5 tags at once are allowed')
-        return Page.objects.filter(id__in=PageTag.objects.filter(tag__name__in=tags).values_list('content_object'))
+class TagViewset(generics.ListAPIView):
+    model = PageTag
+    queryset = PageTag.objects.all()
+    serializer_class = TagSerializer
